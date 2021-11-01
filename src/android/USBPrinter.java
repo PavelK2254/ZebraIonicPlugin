6e127dd9c9d6b6a081f52dc7412034affd06bbf8
pavel4567 <pavel4567@gmail.com>
2021-11-01 11:48:51 +0100

    moved printers to global parameter

--- src/android/USBPrinter.java
+++ src/android/USBPrinter.java
@@ -1,6 +1,5 @@
 package cordova.zebra.plugin;
 
-
 import com.zebra.sdk.comm.Connection;
 import com.zebra.sdk.comm.ConnectionException;
 import com.zebra.sdk.printer.discovery.DiscoveredPrinter;
@@ -27,90 +26,95 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
- public class USBPrinter extends ZebraPlugin{
+public class USBPrinter extends ZebraPlugin {
 
-   private DiscoveredPrinterUsb discoveredPrinterUsb;
-   private UsbManager mUsbManager;
-   private PendingIntent mPermissionIntent;
+    private DiscoveredPrinterUsb discoveredPrinterUsb;
+    private UsbManager mUsbManager;
+    private PendingIntent mPermissionIntent;
+    public List<DiscoveredPrinterUsb> printers;
 
-       public void findPrinters(Context context,final byte[] message) throws RuntimeException{
-                   // Find connected printers
-                   UsbDiscoveryHandler handler = new UsbDiscoveryHandler();
-                   String ACTION_USB_PERMISSION = "com.pk.zebraprintusb.USB_PERMISSION";
-                   IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
-                   mUsbManager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
-                   UsbDiscoverer.findPrinters(context.getApplicationContext(), handler);
-                   mPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
-                   try {
-                       while (!handler.discoveryComplete) {
-                           Thread.sleep(100);
-                       }
+    public void findPrinters(Context context, final byte[] message) throws RuntimeException {
+        // Find connected printers
+        UsbDiscoveryHandler handler = new UsbDiscoveryHandler();
+        String ACTION_USB_PERMISSION = "com.pk.zebraprintusb.USB_PERMISSION";
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        mUsbManager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
+        UsbDiscoverer.findPrinters(context.getApplicationContext(), handler);
+        mPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        try {
+            while (!handler.discoveryComplete) {
+                Thread.sleep(100);
+            }
 
-                       if (handler.printers != null && handler.printers.size() > 0) {
-                         handler.printers.forEach(printer -> {
-                          if(printer != null) discoveredPrinterUsb = printer;
-                        });
-                          if(!mUsbManager.hasPermission(discoveredPrinterUsb.device)){
-                           // throw new RuntimeException("No permission for USB");
-                            mUsbManager.requestPermission(discoveredPrinterUsb.device, mPermissionIntent);
-                          }
-                           printOverUSB(message);
+            if (handler.printers != null && handler.printers.size() > 0) {
+                handler.printers.forEach(printer -> {
+                    if (printer != null)
+                        discoveredPrinterUsb = printer;
+                });
+                if (!mUsbManager.hasPermission(discoveredPrinterUsb.device)) {
+                    // throw new RuntimeException("No permission for USB");
+                    mUsbManager.requestPermission(discoveredPrinterUsb.device, mPermissionIntent);
+                }
+                printOverUSB(message);
 
-                       }else{
-                        throw new RuntimeException("Could not find printers");
-                       }
-                   } catch (Exception e) {
-                       throw new RuntimeException("Zebra plugin exception: " + e);
-                   }
-       }
+            } else {
+                throw new RuntimeException("Could not find printers");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Zebra plugin exception: " + e);
+        }
+    }
 
-       private ZebraImageI getZebraImageFromBitmap(byte[] bitmapByteArray) throws IOException {
-           if(bitmapByteArray == null){
-               throw new RuntimeException("bitmapByteArray is null");
-           }
-           Bitmap mBitmap = BitmapFactory.decodeByteArray(bitmapByteArray,0,bitmapByteArray.length);
-           if(mBitmap == null){
-               throw new IllegalArgumentException("Bitmap is null, bitmapByteArray length: "+ bitmapByteArray.length+ " Bitmap array text: " + Arrays.toString(bitmapByteArray));
-           }
-           return ZebraImageFactory.getImage(mBitmap);
-       }
+    private ZebraImageI getZebraImageFromBitmap(byte[] bitmapByteArray) throws IOException {
+        if (bitmapByteArray == null) {
+            throw new RuntimeException("bitmapByteArray is null");
+        }
+        Bitmap mBitmap = BitmapFactory.decodeByteArray(bitmapByteArray, 0, bitmapByteArray.length);
+        if (mBitmap == null) {
+            throw new IllegalArgumentException("Bitmap is null, bitmapByteArray length: " + bitmapByteArray.length
+                    + " Bitmap array text: " + Arrays.toString(bitmapByteArray));
+        }
+        return ZebraImageFactory.getImage(mBitmap);
+    }
 
-       private void printOverUSB(byte[] bitmapByteArray) throws ConnectionException, ZebraPrinterLanguageUnknownException, IOException {
-               Connection connection = null;
-                   connection = discoveredPrinterUsb.getConnection();
-                   connection.open();
-                   ZebraPrinter printer = ZebraPrinterFactory.getInstance(connection);
-                   if(connection.isConnected()){
-                       int x = 10;
-                       int y = 10;
-                       int width = 0;
-                       int heigth = 0;
-                       printer.printImage(getZebraImageFromBitmap(bitmapByteArray),x,y,width,heigth,false);
-                       connection.close();
-                   }else{
-                       throw new ConnectionException("Could not open connection to a printer");
-                   }
-       }
+    private void printOverUSB(byte[] bitmapByteArray)
+            throws ConnectionException, ZebraPrinterLanguageUnknownException, IOException {
+        Connection connection = null;
+        connection = discoveredPrinterUsb.getConnection();
+        connection.open();
+        ZebraPrinter printer = ZebraPrinterFactory.getInstance(connection);
+        if (connection.isConnected()) {
+            int x = 10;
+            int y = 10;
+            int width = 0;
+            int heigth = 0;
+            printer.printImage(getZebraImageFromBitmap(bitmapByteArray), x, y, width, heigth, false);
+            connection.close();
+        } else {
+            throw new ConnectionException("Could not open connection to a printer");
+        }
+    }
 
+    private class UsbDiscoveryHandler implements DiscoveryHandler {
+        public boolean discoveryComplete = false;
 
-  private class UsbDiscoveryHandler implements DiscoveryHandler {
-          public List<DiscoveredPrinterUsb> printers;
-          public boolean discoveryComplete = false;
+        public UsbDiscoveryHandler() {
+            public List<DiscoveredPrinterUsb> currentPrinters = new LinkedList<DiscoveredPrinterUsb>();
+            if (currentPrinters != null) {
+                printers = currentPrinters;
+            }
+        }
 
-          public UsbDiscoveryHandler() {
-              printers = new LinkedList<DiscoveredPrinterUsb>();
-          }
+        public void foundPrinter(final DiscoveredPrinter printer) {
+            printers.add((DiscoveredPrinterUsb) printer);
+        }
 
-          public void foundPrinter(final DiscoveredPrinter printer) {
-              printers.add((DiscoveredPrinterUsb) printer);
-          }
+        public void discoveryFinished() {
+            discoveryComplete = true;
+        }
 
-          public void discoveryFinished() {
-              discoveryComplete = true;
-          }
-
-          public void discoveryError(String message) {
-              discoveryComplete = true;
-          }
-      }
+        public void discoveryError(String message) {
+            discoveryComplete = true;
+        }
+    }
 }
